d1dcbac8e57ed14ec8ffd300e8980ec0db64e696
Leonardo Coelho <leocoelho.pi@gmail.com>
2024-05-31 22:03:14 -0300

    feat: add table functionalities

--- src/app/components/table/table.component.ts
+++ src/app/components/table/table.component.ts
@@ -1,4 +1,22 @@
-import {Component} from '@angular/core';
+import {AfterViewInit, Component, ViewChild} from '@angular/core';
+import {MatPaginator, MatPaginatorModule} from '@angular/material/paginator';
+import {MatSort, MatSortModule} from '@angular/material/sort';
+import {MatTableDataSource, MatTableModule} from '@angular/material/table';
+import {MatInputModule} from '@angular/material/input';
+import {MatFormFieldModule} from '@angular/material/form-field';
+
+export interface UserData {
+  name: string;
+  status: string;
+  creation_date: Date;
+  last_acess: Date;
+  surname: string;
+  phone: string;
+  email: string;
+  profile: string[];
+  language: string;
+  fav_contact: string;
+}
 
 @Component({
   selector: 'app-table',
@@ -6,5 +24,37 @@ import {Component} from '@angular/core';
   styleUrls: ['./table.component.css']
 })
 export class TableComponent {
-  
+  displayedColumns: string[] = ['user', 'status', 'creation_date', 'last_acess', 'action'];
+  dataSource: MatTableDataSource<UserData>;
+  users: UserData[] = [];
+
+  @ViewChild(MatPaginator) paginator: MatPaginator;
+  @ViewChild(MatSort) sort: MatSort;
+  selectedFilter: string = '';
+
+  constructor() {
+    this.dataSource = new MatTableDataSource(this.users);
+  }
+
+  ngAfterViewInit() {
+    this.dataSource.paginator = this.paginator;
+    this.dataSource.sort = this.sort;
+  }
+
+  applyInputFilter(event: Event) {
+    const filterValue = (event.target as HTMLInputElement).value;
+    this.dataSource.filter = filterValue.trim().toLowerCase();
+
+    if (this.dataSource.paginator) {
+      this.dataSource.paginator.firstPage();
+    }
+  }
+
+  applySelectFilter(filterValue: string): void {
+    this.dataSource.filter = filterValue.trim().toLowerCase();
+  }
+
+  onSelectChange(event: any): void {
+    this.applySelectFilter(event.value);
+  }
 }
